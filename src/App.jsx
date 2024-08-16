5ce1071d8e6f19a6cec1d45d0d32981b2eda943e
Dikshant Sharma <sdikshant89@gmail.com>
2024-08-16 13:34:57 -0600

    Changed basic layout of root elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { Box } from '@mui/material';
+import { blue } from '@mui/material/colors';
 import AppHeader from './components/Header';
 function App() {
   return (
-    <>
+    <Box
+      sx={{
+        flexGrow: 1,
+        margin: 0,
+        padding: 0,
+        backgroundColor: blue,
+      }}
+      classname="parentBox"
+    >
       <AppHeader />
       <p>Click on the Vite and React logos to learn more</p>
-    </>
+    </Box>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -7,15 +6,13 @@ import * as React from 'react';
 
 export default function AppHeader() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h1" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
   );
 }
